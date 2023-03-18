85691dde6ce8861e52798d3abd33520014726b5f
Igorttdp <igorttdp.dev@email.com>
2023-03-18 11:57:14 -0300

    feat: add new view 'MovieView'
    introduces the use of generic ListCreateAPIView

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,13 @@
 from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .serializers import MovieSerializer
+from .models import Movie
 
 
 class MovieView(generics.ListCreateAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Movie
+    serializer_class = MovieSerializer
