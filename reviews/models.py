9165a3eb2464f660265ba880dc4b429c0ec8fe33
Igorttdp <igorttdp.dev@email.com>
2023-03-20 12:22:45 -0300

    feat: add 'Review' model and their migrations

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from uuid import uuid4
 
 
 class Review(models.Model):
-    ...
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    stars = models.IntegerField()
+    review = models.TextField()
+    spoilers = models.BooleanField(default=False)
+
+    movie = models.ForeignKey("movies.Movie", related_name="reviews", on_delete=models.CASCADE)
+    critic = models.ForeignKey("users.User", related_name="reviews", on_delete=models.CASCADE)
