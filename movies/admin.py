6e6ada5da3ddced609ad5f2656c66d1921c09b03
Igorttdp <igorttdp.dev@email.com>
2023-04-13 15:59:16 -0300

    feat: add Movie model for admin site

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+class CustomMoviesAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "duration",
+        "premiere",
+        "budget",
+        "overview",
+        "user",
+    ]
+
+    ordering = ["title", "duration"]
+
+
+admin.site.register(Movie, CustomMoviesAdmin)
